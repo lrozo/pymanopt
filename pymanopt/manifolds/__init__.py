684df6c2816a4d8e8d6a31143a2c61ac5bfc6a0d
Rozo Leonel (CR/PJ-AI-R31) <leonel.rozo@de.bosch.com>
2021-05-31 20:39:57 +0200

    Added files for hyperbolic manifold.

--- pymanopt/manifolds/__init__.py
+++ pymanopt/manifolds/__init__.py
@@ -17,7 +17,8 @@ __all__ = [
     "Stiefel",
     "StrictlyPositiveVectors",
     "Symmetric",
-    "SymmetricPositiveDefinite"
+    "SymmetricPositiveDefinite",
+    "PoincareBall",
 ]
 
 from .complex_circle import ComplexCircle
@@ -25,12 +26,20 @@ from .complex_grassmann import ComplexGrassmann
 from .euclidean import Euclidean, SkewSymmetric, Symmetric
 from .fixed_rank import FixedRankEmbedded
 from .grassmann import Grassmann
+from .hyperbolic import PoincareBall
 from .oblique import Oblique
 from .product import Product
-from .psd import (Elliptope, PSDFixedRank, PSDFixedRankComplex,
-                  SymmetricPositiveDefinite)
+from .psd import (
+    Elliptope,
+    PSDFixedRank,
+    PSDFixedRankComplex,
+    SymmetricPositiveDefinite,
+)
 from .special_orthogonal_group import SpecialOrthogonalGroup
-from .sphere import (Sphere, SphereSubspaceComplementIntersection,
-                     SphereSubspaceIntersection)
+from .sphere import (
+    Sphere,
+    SphereSubspaceComplementIntersection,
+    SphereSubspaceIntersection,
+)
 from .stiefel import Stiefel
 from .strictly_positive_vectors import StrictlyPositiveVectors
